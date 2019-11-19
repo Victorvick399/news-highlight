c85ad9eb7b0ed9cec8a2d60d50be75b10ad50de0
Victorvick399 <victorvick399@gmail.com>
2019-11-19 07:30:38 +0300

    create the configurations containing the urls for obtaining data from the api

---
+++ config.py
@@ -0,0 +1,34 @@
+import os
+
+class Config:
+	"""
+	General configuration parent class
+	"""
+	API_KEY=os.environ.get('API_KEY')
+	SOURCE_URL='https://newsapi.org/v2/top-headlines?sources={}&apiKey={}'
+	HEADLINES_SEARCH_URL='https://newsapi.org/v2/top-headlines?category={}&query={}&apiKey={}'
+	HEADLINES_URL='https://newsapi.org/v2/everything?q={}&apiKey={}'
+	EVERYTHING_URL='https://newsapi.org/v2/sources?language=en&category={}&apiKey={}'
+	SECRET_KEY=os.environ.get('SECRET_KEY')
+	
+
+class ProdConfig(Config):
+	"""
+	Production configuration child class
+	Args:
+		Config:The parent configuration class with general configuration settings
+	"""
+	pass
+
+class DevConfig(Config):
+	"""
+	Development configuration child class
+	Args:
+		Config: the parent configuration class with general configuration settings
+	"""
+	DEBUG= True
+
+config_options  = {
+'development':DevConfig,
+'production':ProdConfig
+}
